4a16640daeb3ee94c9ec8f24041ab0c9adf45151
shailstar <shailstar201595@gmail.com>
2024-04-12 23:45:09 +0530

    enc: store-created

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,11 @@
       :height="`${100 / 2.45}vw`"
     />
     <div class="flex flex-col gap-2">
-      <CategorySection v-for="(category, index) in categoryData" :key="'category' + index" :value="category"/>
+      <CategorySection
+        v-for="(category, index) in categoryData"
+        :key="'category' + index"
+        :value="category"
+      />
     </div>
   </div>
 </template>
@@ -20,192 +24,11 @@
 <script setup>
 import Carousel from "./components/Carousel/index.vue";
 import CategorySection from "./components/CategorySection/index.vue";
+import { initCarouselData, initCategoryData } from "./store/data";
 import { ref } from "vue";
-import banner1 from "./assets/banners/banner1.jpg";
-import banner2 from "./assets/banners/banner2.jpg";
-import banner3 from "./assets/banners/banner7.jpg";
-import banner4 from "./assets/banners/banner4.jpg";
-import banner5 from "./assets/banners/banner5.jpg";
-import banner6 from "./assets/banners/banner6.jpg";
-import poster1 from "./assets/posters/poster1.png"
-import poster2 from "./assets/posters/poster2.png"
-import poster3 from "./assets/posters/poster3.png"
-import poster4 from "./assets/posters/poster4.png"
-import poster5 from "./assets/posters/poster5.png"
-import poster6 from "./assets/posters/poster6.png"
-import poster7 from "./assets/posters/poster7.png"
-import poster8 from "./assets/posters/poster8.png"
-import poster9 from "./assets/posters/poster9.png"
-import poster10 from "./assets/posters/poster10.png"
-import poster11 from "./assets/posters/poster11.png"
-import poster12 from "./assets/posters/poster12.png"
-import poster13 from "./assets/posters/poster13.png"
 
-
-const initCarouselData = [
-  {
-    id: "1000",
-    code: "f230fh0g3",
-    name: "Bamboo Watch",
-    description: "Product Description",
-    image: banner1,
-    price: 65,
-    category: "Accessories",
-    quantity: 24,
-    inventoryStatus: "INSTOCK",
-    rating: 5,
-  },
-  {
-    id: "1001",
-    code: "f230fh0g3",
-    name: "Bamboo Watch",
-    description: "Product Description",
-    image: banner2,
-    price: 65,
-    category: "Accessories",
-    quantity: 24,
-    inventoryStatus: "INSTOCK",
-    rating: 5,
-  },
-  {
-    id: "1002",
-    code: "f230fh0g3",
-    name: "Bamboo Watch",
-    description: "Product Description",
-    image: banner3,
-    price: 65,
-    category: "Accessories",
-    quantity: 24,
-    inventoryStatus: "INSTOCK",
-    rating: 5,
-  },
-  {
-    id: "1003",
-    code: "f230fh0g3",
-    name: "Bamboo Watch",
-    description: "Product Description",
-    image: banner4,
-    price: 65,
-    category: "Accessories",
-    quantity: 24,
-    inventoryStatus: "INSTOCK",
-    rating: 5,
-  },
-  {
-    id: "1004",
-    code: "f230fh0g3",
-    name: "Bamboo Watch",
-    description: "Product Description",
-    image: banner5,
-    price: 65,
-    category: "Accessories",
-    quantity: 24,
-    inventoryStatus: "INSTOCK",
-    rating: 5,
-  },
-];
-
-const initCategoryData = [
-  {
-    name: "Trending",
-    posters: [
-      {
-        name: "About My Father",
-        image: poster1
-      },
-    ],
-  },
-  {
-    name: "Best in Horror",
-    posters: [
-      {
-        name: "Barbie",
-        image: poster2,
-      },
-      {
-        name: "It Lives Inside",
-        image: poster3
-      },
-      {
-        name: "Infinity Pool",
-        image: poster4
-      },
-      {
-        name: "Killers of the Flower Moon",
-        image: poster5
-      },
-    ],
-  },
-  {
-    name: "Most-watched period films",
-    posters: [
-      {
-        name: "Oppenhimer",
-        image: poster6
-      },
-      {
-        name: "Past Lives",
-        image: poster7
-      },
-      {
-        name: "Polite Society",
-        image: poster8
-      },
-    ],
-  },
-  {
-    name: "All Titles",
-    posters: [
-      {
-        name: "Oppenhimer",
-        image: poster9
-      },
-      {
-        name: "Past Lives",
-        image: poster10
-      },
-      {
-        name: "Polite Society",
-        image: poster11
-      },
-      {
-        name: "Barbie",
-        image: poster12
-      },
-      {
-        name: "It Lives Inside",
-        image: poster13
-      },
-      {
-        name: "Infinity Pool",
-        image: poster1
-      },
-      {
-        name: "Killers of the Flower Moon",
-        image: poster2
-      },
-      {
-        name: "Barbie",
-        image: poster12
-      },
-      {
-        name: "It Lives Inside",
-        image: poster13
-      },
-      {
-        name: "Infinity Pool",
-        image: poster1
-      },
-      {
-        name: "Killers of the Flower Moon",
-        image: poster2
-      },
-    ],
-  },
-];
-
-const carouselData = ref(initCarouselData);
-const categoryData = ref(initCategoryData);
+const carouselData = ref(initCarouselData());
+const categoryData = ref(initCategoryData());
 </script>
 
 <style scoped></style>
